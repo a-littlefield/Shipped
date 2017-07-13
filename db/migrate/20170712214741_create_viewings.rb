bd0f923fd21d4913357ef85ff455d53cc16a25fd
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-07-13 09:56:50 -0600

    Created app and user can log in

---
+++ db/migrate/20170712214741_create_viewings.rb
@@ -0,0 +1,12 @@
+class CreateViewings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :viewings do |t|
+      t.references :user, foreign_key: true
+      t.references :movie, foreign_key: true
+      t.integer :watched_count
+      t.boolean :liked
+
+      t.timestamps
+    end
+  end
+end
